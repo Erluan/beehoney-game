34d53be2a85d4d4a78508afc5c697ce422b835bb
Erluan Felix Batista <erluan@gmail.com>
2022-02-24 20:12:58 -0300

    :hammer: updating Sprites

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
+import 'package:beehoney/Classes/bg.dart';
+import 'package:flame/components.dart';
 import 'package:flame/game.dart';
+import 'package:flame/input.dart';
 import 'package:flutter/material.dart';
 
 Future<void> main() async {
@@ -7,6 +10,58 @@ Future<void> main() async {
   );
 }
 
-class BeeHoney extends FlameGame {
+class BeeHoney extends FlameGame with KeyboardEvents {
+
+  Bg bg = Bg();
+  Bg bg2 = Bg();
+  SpriteComponent bee = SpriteComponent();
+
+  @override
+  Future<void>? onLoad() async {
+    bg
+      ..sprite = await Sprite.load("bg.png")
+      ..size.x = 415
+      ..size.y = 730
+      ..position = Vector2(0, 0);
+
+    add(bg);
+
+    bg2
+      ..sprite = await Sprite.load("bg.png")
+      ..size.x = 415
+      ..size.y = 730
+      ..position = Vector2(0, -730);
+
+    add(bg2);
+
+    bee
+      ..sprite = await Sprite.load("bee1.png")
+      ..size = Vector2.all(50)
+      ..position = Vector2(250, 250)
+      ..anchor = Anchor.center;
+
+    add(bee);
+    return super.onLoad();
+  }
+
+  @override
+  void update(double dt) {
+    bg.move(dt, 100, 730, 0);
+    bg2.move(dt, 100, 0, -730);
+    super.update(dt);
+
+  }
+
+  // @override
+  // KeyEventResult onKeyEvent(RawKeyEvent event, Set<LogicalKeyboardKey> keysPressed) {
+  //   if(event.data.keyLabel == "a") {
+  //     bee.x -= 1;
+  //   }
+  //   if(event.data.keyLabel == "d") {
+  //     bee.x += 1;
+  //   }
+  //   return super.onKeyEvent(event, keysPressed);
+  // }
 
 }
+
